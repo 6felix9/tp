f6424eec31bf99fdb41dcf4abd01dcf9e7acb711
VincentJulijanto <e1375559@u.nus.edu>
2026-03-12 20:22:31 +0800

    Fix checkstyle violation in delete command message

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -19,7 +19,8 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the athlete profile identified by the index number used in the displayed athlete profile list.\n"
+            + ": Deletes the athlete profile identified by the index number used "
+            + "in the displayed athlete profile list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
